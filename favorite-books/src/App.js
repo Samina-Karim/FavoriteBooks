e337e6070a7a670b2ebe366db16210fd697adb98
Samina-Karim <karim.samina@gmail.com>
2023-11-14 18:49:26 -0500

    Step 1: Initial Setup

---
+++ favorite-books/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Favorite Books</h1>
+    <p>This is a Book List application where users can add and remove books from their book list</p>
+    </>
+  );
+}
+
+export default App;
